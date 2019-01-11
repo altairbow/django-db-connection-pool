8b3128cd05cee279884fb9d1050bf56eaf6adb1c
Altair Bow <altair.bow@foxmail.com>
2019-01-11 09:49:21 +0800

    Add initial setup script

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+from dj_db_conn_pool import (
+    __version__,
+    __author__,
+    __author_email__,
+    __description__
+)
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='django-db-connection-pool',
+        license='MIT',
+        version=__version__,
+        description=__description__,
+        long_description=open('README.md').read(),
+        author=__author__,
+        author_email=__author_email__,
+        url='https://github.com/altairbow/django-db-connection-pool',
+        download_url='https://pypi.python.org/pypi/django-db-connection-pool/',
+        packages=[
+            'dj_db_conn_pool',
+            'dj_db_conn_pool.backends',
+            'dj_db_conn_pool.backends.mysql',
+            'dj_db_conn_pool.backends.oracle'
+        ],
+        include_package_data=True,
+        install_requires=[
+            'SQLAlchemy>=1.2.15'
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        keywords=['django', 'db', 'database', 'persistent', 'connection', 'pool'],
+    )
