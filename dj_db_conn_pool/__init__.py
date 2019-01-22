62d0aae226ece64430782d4da51c0caa0836f301
Altair Bow <altair.bow@foxmail.com>
2019-01-22 09:27:20 +0800

    increase version

--- dj_db_conn_pool/__init__.py
+++ dj_db_conn_pool/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __author__ = 'Altair Bow',
 __author_email__ = 'altair.bow@foxmail.com'
 __description__ = 'Persistent database connection backends for Django'
