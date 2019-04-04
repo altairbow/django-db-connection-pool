6401baf9a8482ee5ad97d8c34d758ffccbd5eb82
xueguanwen <xgw1989@sina.com>
2019-04-04 09:11:49 +0800

    fix bug
    fix this:
    ```
        lambda: super(BaseDatabaseWrapper, self).get_new_connection(conn_params),
    TypeError: must be type, not classobj
    ```

--- dj_db_conn_pool/backends/oracle/base.py
+++ dj_db_conn_pool/backends/oracle/base.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db.backends.oracle import base
 from cx_Oracle import DatabaseError
 from sqlalchemy.dialects.oracle.cx_oracle import OracleDialect
@@ -7,7 +9,7 @@ from dj_db_conn_pool.backends import BaseDatabaseWrapper
 class Dialect(OracleDialect):
     def do_ping(self, dbapi_connection):
         try:
-            return super().do_ping(dbapi_connection)
+            return super(OracleDialect, self).do_ping(dbapi_connection)
         except DatabaseError:
             return False
 
