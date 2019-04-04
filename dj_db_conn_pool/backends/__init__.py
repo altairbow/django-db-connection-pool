e1da79e802fc861ff9de98a55cb8b2732cfb89d6
xueguanwen <xgw1989@sina.com>
2019-04-04 09:14:04 +0800

    Update __init__.py

--- dj_db_conn_pool/backends/__init__.py
+++ dj_db_conn_pool/backends/__init__.py
@@ -20,7 +20,7 @@ pool_default_params = {
 }
 
 
-class BaseDatabaseWrapper:
+class BaseDatabaseWrapper(object):
     def get_new_connection(self, conn_params):
         """
         覆盖 Django 的 get_new_connection 方法
