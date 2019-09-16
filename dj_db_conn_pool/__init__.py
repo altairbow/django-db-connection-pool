5064903df9d8f7548c76904eb1ee6fff75fa9907
Altair Bow <altair.bow@foxmail.com>
2019-09-16 11:10:22 +0800

    add dj_db_conn_pool.setup, for setting up default parameters

--- dj_db_conn_pool/__init__.py
+++ dj_db_conn_pool/__init__.py
@@ -1,4 +1,13 @@
-__version__ = '1.0.1'
-__author__ = 'Altair Bow',
+__version__ = '1.0.2'
+__author__ = 'Altair Bow'
 __author_email__ = 'altair.bow@foxmail.com'
 __description__ = 'Persistent database connection backends for Django'
+
+
+def setup(pool_size=10, max_overflow=10):
+    from dj_db_conn_pool.core import pool_container
+
+    pool_container.pool_default_params.update(
+        pool_size=pool_size,
+        max_overflow=max_overflow
+    )
