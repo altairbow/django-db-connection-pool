7853d889746e490bd598e9319003aa810b11b2ab
Altair Bow <altair.bow@foxmail.com>
2020-05-25 14:46:24 +0800

    更新 dj_db_conn_pool.setup：支持更多默认参数的设置

--- dj_db_conn_pool/__init__.py
+++ dj_db_conn_pool/__init__.py
@@ -1,13 +1,14 @@
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __author__ = 'Altair Bow'
 __author_email__ = 'altair.bow@foxmail.com'
 __description__ = 'Persistent database connection backends for Django'
 
 
-def setup(pool_size=10, max_overflow=10):
+def setup(**kwargs):
     from dj_db_conn_pool.core import pool_container
 
-    pool_container.pool_default_params.update(
-        pool_size=pool_size,
-        max_overflow=max_overflow
-    )
+    for key, value in kwargs.items():
+        if key in pool_container.pool_default_params:
+            pool_container.pool_default_params.update(
+                key=value
+            )
