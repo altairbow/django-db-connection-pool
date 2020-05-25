6e796b7b4b2dfdeeaa90b07f7d71192c04219667
Altair Bow <altair.bow@foxmail.com>
2020-05-25 14:22:41 +0800

    change SQLAlchemyDialect to sqlalchemy.dialects.mysql.pymysql.MySQLDialect_pymysql

--- dj_db_conn_pool/backends/mysql/base.py
+++ dj_db_conn_pool/backends/mysql/base.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.db.backends.mysql import base
-from sqlalchemy.dialects.mysql.base import MySQLDialect
+from sqlalchemy.dialects.mysql.pymysql import MySQLDialect_pymysql
 from dj_db_conn_pool.core.mixins import PooledDatabaseWrapperMixin
 
 
 class DatabaseWrapper(PooledDatabaseWrapperMixin, base.DatabaseWrapper):
-    class SQLAlchemyDialect(MySQLDialect):
-        def _extract_error_code(self, exception):
-            return exception.args[0]
+    SQLAlchemyDialect = MySQLDialect_pymysql
