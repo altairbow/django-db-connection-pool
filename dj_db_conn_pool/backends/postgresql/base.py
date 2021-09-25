ae5737627aaf603696127a5e7c98abad61628c2f
Richard Kojedzinszky <richard@kojedz.in>
2021-09-25 12:02:15 +0200

    postgres: use psycopg2 dialect

--- dj_db_conn_pool/backends/postgresql/base.py
+++ dj_db_conn_pool/backends/postgresql/base.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.db.backends.postgresql import base
-from sqlalchemy.dialects.postgresql.base import PGDialect
+from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
 from dj_db_conn_pool.core.mixins import PooledDatabaseWrapperMixin
 
 
 class DatabaseWrapper(PooledDatabaseWrapperMixin, base.DatabaseWrapper):
-    class SQLAlchemyDialect(PGDialect):
+    class SQLAlchemyDialect(PGDialect_psycopg2):
         pass
