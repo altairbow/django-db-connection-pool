a8815daf987a9b12b66b0e9fb51f821deb3c2e7c
Richard Kojedzinszky <richard@kojedz.in>
2021-10-05 14:21:06 +0200

    PooledDatabaseWrapperMixin: proxy autocommit setting to dbapi object

--- dj_db_conn_pool/backends/postgresql/base.py
+++ dj_db_conn_pool/backends/postgresql/base.py
@@ -8,3 +8,7 @@ from dj_db_conn_pool.core.mixins import PooledDatabaseWrapperMixin
 class DatabaseWrapper(PooledDatabaseWrapperMixin, base.DatabaseWrapper):
     class SQLAlchemyDialect(PGDialect_psycopg2):
         pass
+
+    def _set_autocommit(self, autocommit):
+        with self.wrap_database_errors:
+            self.connection.dbapi_connection.autocommit = autocommit
