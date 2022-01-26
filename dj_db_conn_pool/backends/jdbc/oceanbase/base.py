4566e51b04e37a9b6cd5a0c93f497d4e7eb402c2
Altair Bow <altair.bow@foxmail.com>
2022-01-26 10:46:32 +0800

    update OceanBase backend

--- dj_db_conn_pool/backends/jdbc/oceanbase/base.py
+++ dj_db_conn_pool/backends/jdbc/oceanbase/base.py
@@ -1,1 +1,28 @@
 # -*- coding: utf-8 -*-
+
+import jpype
+import jaydebeapi
+from django.db.backends.oracle import base
+from sqlalchemy.dialects.oracle.base import OracleDialect
+from dj_db_conn_pool.backends.jdbc import JDBCDatabaseWrapper
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class DatabaseWrapper(JDBCDatabaseWrapper, base.DatabaseWrapper):
+    class SQLAlchemyDialect(OracleDialect):
+        def do_ping(self, dbapi_connection):
+            try:
+                return super(OracleDialect, self).do_ping(dbapi_connection)
+            except (jaydebeapi.DatabaseError, jpype.JException):
+                return False
+
+    JDBC_DRIVER = 'com.alipay.oceanbase.jdbc.Driver'
+
+    def _get_jdbc_url(self):
+        return 'jdbc:oceanbase://{NAME}'.format(**self.settings_dict)
+
+    def init_connection_state(self):
+        pass
