b704e8979434edd605aa930eff30e133cf186cab
Altair Bow <altair.bow@foxmail.com>
2022-04-12 09:17:19 +0800

    jdbc backends: add support for named parameters

--- dj_db_conn_pool/backends/jdbc/__init__.py
+++ dj_db_conn_pool/backends/jdbc/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import types
+import sqlparams
 import jaydebeapi
 from dj_db_conn_pool.core.mixins import PooledDatabaseWrapperMixin
 
 import logging
 logger = logging.getLogger(__name__)
+
+sql_params = sqlparams.SQLParams('named', 'qmark')
 
 
 class JDBCDatabaseWrapper(PooledDatabaseWrapperMixin):
@@ -37,7 +40,7 @@ class JDBCDatabaseWrapper(PooledDatabaseWrapperMixin):
     def create_cursor(self, name=None):
         """
         create a cursor
-        just for compatibility
+        do some tricks here
         :param name:
         :return:
         """
@@ -45,19 +48,35 @@ class JDBCDatabaseWrapper(PooledDatabaseWrapperMixin):
         cursor = super().create_cursor(name)
 
         # just for compatibility
-        cursor.setinputsizes = types.MethodType(lambda *_: None, cursor)
+        # cursor.setinputsizes = types.MethodType(lambda *_: None, cursor)
 
-        def _execute(_self, query, *_args):
-            # replace placeholder
-            query = query.replace('%s', '?')
+        def _execute(_self, query, parameters=None):
+            """
+            :param _self: django's cursor
+            :param query: SQL
+            :param parameters: SQL parameters
+            :return:
+            """
+            if isinstance(parameters, dict):
+                # convert sql and parameters
+                _query, _parameters = sql_params.format(query, parameters)
+
+                logger.debug(
+                    'SQL (%s), parameters(%s) has been converted to SQL(%s), parameters(%s)',
+                    query, parameters, _query, _parameters)
+
+                query, parameters = (_query, _parameters)
+            else:
+                # change paramstyle 'format' to 'qmark'
+                query = query.replace('%s', '?')
 
             # record last query
             cursor.statement = query
 
             # call jaydebeapi
-            _self.cursor.execute(query, *_args)
+            _self.cursor.execute(query, parameters)
 
-        # just for compatibility
+        # replace django cursor's execute method
         cursor.execute = types.MethodType(_execute, cursor)
 
         return cursor
