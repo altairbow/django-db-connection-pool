a2097d80dc344e35d07ffe14a81364add3b1ee11
Altair Bow <altair.bow@foxmail.com>
2022-06-30 18:03:48 +0800

    fix mysql autocommit

--- dj_db_conn_pool/backends/mysql/base.py
+++ dj_db_conn_pool/backends/mysql/base.py
@@ -4,7 +4,17 @@ from django.db.backends.mysql import base
 from sqlalchemy.dialects.mysql.pymysql import MySQLDialect_pymysql
 from dj_db_conn_pool.core.mixins import PooledDatabaseWrapperMixin
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class DatabaseWrapper(PooledDatabaseWrapperMixin, base.DatabaseWrapper):
     class SQLAlchemyDialect(MySQLDialect_pymysql):
         server_version_info = (0, )
+
+    def _set_autocommit(self, autocommit):
+        with self.wrap_database_errors:
+            try:
+                self.connection.connection.autocommit(autocommit)
+            except (Exception, ) as e:
+                logger.exception('unable to set database(%s) autocommit: %s', self.alias, e)
