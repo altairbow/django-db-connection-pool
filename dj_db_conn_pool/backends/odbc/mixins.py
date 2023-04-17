d65ce7aec6a4802a32a8ad9c8e126ae442fbf3af
Altair Bow <altair.bow@foxmail.com>
2023-04-17 15:52:00 +0800

    update odbc

--- dj_db_conn_pool/backends/odbc/mixins.py
+++ dj_db_conn_pool/backends/odbc/mixins.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import pyodbc
+from django.conf import ImproperlyConfigured
 from dj_db_conn_pool.core.mixins import PersistentDatabaseWrapperMixin
 
 
 class ODBCWrapperMixin(PersistentDatabaseWrapperMixin):
     def _get_new_connection(self, conn_params):
-        conn = pyodbc.connect(
-            '', **self.settings_dict
-        )
+        try:
+            driver = self.settings_dict['ODBC_OPTIONS']['DRIVER']
+        except KeyError:
+            raise ImproperlyConfigured('No odbc driver provided')
 
-        return conn
+        conn_str_template = 'DRIVER={DRIVER};SERVER={HOST}:{PORT};DATABASE={NAME};UID={USER};PWD={PASSWORD}'
+
+        connection_string = conn_str_template.format(DRIVER=driver, **self.settings_dict)
+
+        return pyodbc.connect(connection_string)
