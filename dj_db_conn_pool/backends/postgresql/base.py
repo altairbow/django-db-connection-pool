7c4e509295242e6ca4eb2cc2e77d0147b928df71
Altair Bow <altair.bow@foxmail.com>
2023-05-25 15:57:26 +0800

    fix #47

--- dj_db_conn_pool/backends/postgresql/base.py
+++ dj_db_conn_pool/backends/postgresql/base.py
@@ -5,4 +5,10 @@ from dj_db_conn_pool.backends.postgresql.mixins import PGDatabaseWrapperMixin
 
 
 class DatabaseWrapper(PGDatabaseWrapperMixin, base.DatabaseWrapper):
-    pass
+    def get_new_connection(self, conn_params):
+        connection = super().get_new_connection(conn_params)
+
+        if not connection.info:
+            connection.info = connection.connection.info
+
+        return connection
