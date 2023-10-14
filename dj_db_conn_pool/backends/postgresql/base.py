557d19ecd48c38cfccb02111137c8d54e14db041
Altair Bow <altair.bow@foxmail.com>
2023-10-14 18:49:27 +0800

    postgres: replace `connection.info` with `connection.connection.info`

--- dj_db_conn_pool/backends/postgresql/base.py
+++ dj_db_conn_pool/backends/postgresql/base.py
@@ -6,10 +6,4 @@ from dj_db_conn_pool.backends.postgresql.mixins import PGDatabaseWrapperMixin
 
 
 class DatabaseWrapper(PGDatabaseWrapperMixin, base.DatabaseWrapper):
-    def get_new_connection(self, conn_params):
-        connection = super().get_new_connection(conn_params)
-
-        if not connection.info:
-            connection.info = connection.connection.info
-
-        return connection
+    pass
--- dj_db_conn_pool/backends/postgresql/mixins.py
+++ dj_db_conn_pool/backends/postgresql/mixins.py
@@ -8,3 +8,11 @@ from dj_db_conn_pool.core.mixins import PersistentDatabaseWrapperMixin
 class PGDatabaseWrapperMixin(PersistentDatabaseWrapperMixin):
     class SQLAlchemyDialect(PGDialect_psycopg2):
         pass
+
+    def get_new_connection(self, conn_params):
+        connection = super().get_new_connection(conn_params)
+
+        if not connection.info:
+            connection.info = connection.connection.info
+
+        return connection
