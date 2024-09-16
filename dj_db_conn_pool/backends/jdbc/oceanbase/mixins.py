937deff33fad7ecc40474496202f9bc843b8e572
Altair Bow <altair.bow@foxmail.com>
2024-09-16 17:52:31 +0800

    jdbc: Modify ob driver name

--- dj_db_conn_pool/backends/jdbc/oceanbase/mixins.py
+++ dj_db_conn_pool/backends/jdbc/oceanbase/mixins.py
@@ -4,6 +4,6 @@ from dj_db_conn_pool.backends.jdbc.mixins import JDBCDatabaseWrapperMixin
 class JDBCOceanBaseDatabaseWrapperMixin(JDBCDatabaseWrapperMixin):
     vendor = 'OceanBase'
 
-    jdbc_driver = 'com.oceanbase.jdbc.driver'
+    jdbc_driver = 'com.oceanbase.jdbc.Driver'
 
     jdbc_url_prefix = 'jdbc:oceanbase:'
